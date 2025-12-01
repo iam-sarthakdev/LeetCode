94da93d729092ef61e3ad0d01e931882d04c7c1a
Sarthak Kanoi <sarthak1712005@gmail.com>
2025-12-01 23:01:01 +0530

    Time: 3 ms (72.53%) | Memory: 43.1 MB (40.38%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else { // closing 
+                if (st.isEmpty() == true)
+                    return false;
+                char top = st.pop();
+
+                if (c == ')' && top != '(')
+                    return false;
+                if (c == ']' && top != '[')
+                    return false;
+                if (c == '}' && top != '{')
+                    return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}

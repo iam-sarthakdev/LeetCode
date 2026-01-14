babbe9b5c86e2839c8d785f2a3df8b7f415d113f
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-01-14 22:51:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (61.12%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,22 +1,18 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack<>();
-        int minMoves = 0;
+        int open = 0;
+        int close = 0;
 
         for(char c : s.toCharArray()){
-            if(c == '('){ // just push into stacks
-            st.push(c);
-            }else{ // c is ')'
-            if(st.isEmpty()){
-                minMoves++;
-            }else{ // stack is not empty
-            if(!st.isEmpty() && st.peek() == '('){
-                st.pop();
-            }
+            if(c == '('){
+                open++;
+            }else{ // close
+            if(open > 0) open--;
+            else{
+                close++;
             }
             }
         }
-        minMoves += st.size();
-        return minMoves;
+        return open + close;
     }
 }

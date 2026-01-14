878a56c18c399ab35daa508e91b02f30ea9ca5ba
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-01-15 01:37:24 +0530

    Stacks

---
+++ LeetCode/Medium/0948-bag-of-tokens/0948-bag-of-tokens-01-15-2026-01-37-24.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power){
+        Arrays.sort(tokens);
+        int currScore = 0;
+        int maxScore = 0;
+
+        int left = 0;
+        int right = tokens.length - 1;
+
+        while(right >= left){
+            if(tokens[left] <= power){
+                power -= tokens[left];
+                currScore++;
+                maxScore = Math.max(maxScore, currScore);
+                left++;
+            }else if(tokens[left] + tokens[right] <= power && left != right){
+                currScore += 2;
+                maxScore = Math.max(maxScore, currScore);
+                left++;
+                right--;
+            }else if(currScore == 0){
+                if(power >= tokens[left]){
+                    power -= tokens[left];
+                    currScore++;
+                    maxScore = Math.max(maxScore, currScore);
+                    left++;
+                }else return maxScore;
+                }else if(currScore > 0){
+                    power += tokens[right];
+                    currScore--;
+                    right--;
+                }
+            }
+            return maxScore;
+        }
+    }

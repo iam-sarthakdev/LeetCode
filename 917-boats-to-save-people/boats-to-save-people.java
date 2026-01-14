e704bb0a8f71348bf398093692fd4c8173ee3e01
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-01-14 22:16:17 +0530

    Time: 20 ms (80.58%) | Memory: 56.8 MB (13.52%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -5,24 +5,19 @@ class Solution {
        int left = 0;
        int right = people.length - 1;
 
-       while(right > left){
+       while(right >= left){
         if(people[right] == limit){
-            people[right] = -people[right];
             boats++;
             right--;
         }else if(people[left] + people[right] <= limit){
-            people[left] = -people[left];
-            people[right] = -people[right];
             boats++;
             left++;
             right--;
-        }else if(people[left] + people[right] > limit){
+        }else{
             right--;
+            boats++;
         }
-       }
-       for(int i = 0; i < people.length; i++){
-        if(people[i] > 0) boats++;
-       }
+        }
        return boats;
     }
 }

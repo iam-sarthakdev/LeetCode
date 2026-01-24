5692daed94ced6ddde17ed4e7a73ecec71a00453
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-01-24 22:25:05 +0530

    Time: 1 ms (81.78%) | Memory: 43.8 MB (51.59%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -18,7 +18,7 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         if(root == null)return res;
         boolean leftToRight = true;
-        Queue<TreeNode> q = new LinkedList<>();
+        Queue<TreeNode> q = new ArrayDeque<>();
         
         q.offer(root);
         while(!q.isEmpty()){
@@ -28,8 +28,8 @@ class Solution {
                 TreeNode temp = q.peek();
                 if(temp.left != null) q.offer(temp.left);
                 if(temp.right != null)q.offer(temp.right);
-                if(leftToRight) level.addLast(q.poll().val);
-                else if(!leftToRight) level.addFirst(q.poll().val);
+                if(leftToRight) level.offerLast(q.poll().val);
+                else if(!leftToRight) level.offerFirst(q.poll().val);
             }
             res.add(new ArrayList<>(level));
             leftToRight = !leftToRight;

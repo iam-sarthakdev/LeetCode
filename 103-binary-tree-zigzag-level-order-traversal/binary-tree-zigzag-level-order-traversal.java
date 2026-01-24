f5b73c2adc0bd5a122e92aafec0ea33abcc75ce2
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-01-24 22:11:47 +0530

    Time: 1 ms (81.78%) | Memory: 43.5 MB (92.92%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,27 +1,39 @@
-import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<LinkedList<Integer>> result = new ArrayList<>();
-        dfs(root, 0, result);
-        return new ArrayList<>(result);
-    }
-
-    private void dfs(TreeNode node, int level, List<LinkedList<Integer>> result) {
-        if (node == null) return;
-
-        // if current level list does not exist, create it
-        if (result.size() == level) {
-            result.add(new LinkedList<>());
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null)return res;
+        boolean leftToRight = true;
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.offer(root);
+        while(!q.isEmpty()){
+            Deque<Integer> level = new ArrayDeque<>();
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode temp = q.peek();
+                if(temp.left != null) q.offer(temp.left);
+                if(temp.right != null)q.offer(temp.right);
+                if(leftToRight) level.addLast(q.poll().val);
+                else if(!leftToRight) level.addFirst(q.poll().val);
+            }
+            res.add(new ArrayList<>(level));
+            leftToRight = !leftToRight;
         }
-
-        if (level % 2 == 0) {
-            result.get(level).addLast(node.val);   // left → right
-        } else {
-            result.get(level).addFirst(node.val);  // right → left
-        }
-
-        dfs(node.left, level + 1, result);
-        dfs(node.right, level + 1, result);
+        return res;
     }
 }

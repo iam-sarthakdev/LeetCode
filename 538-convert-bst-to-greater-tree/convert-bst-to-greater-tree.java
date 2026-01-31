f93c4c8cd571bdbafea21804d672329c85dd6eaf
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-01-31 12:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (94.09%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
@@ -0,0 +1,18 @@
+class Solution {
+    int prefixSum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        return postorder(root);
+    }
+    private TreeNode postorder(TreeNode root){
+        if(root == null) return null;
+
+        postorder(root.right);
+
+        root.val += prefixSum;
+        prefixSum = root.val;
+
+        postorder(root.left);
+
+        return root;
+    }
+}

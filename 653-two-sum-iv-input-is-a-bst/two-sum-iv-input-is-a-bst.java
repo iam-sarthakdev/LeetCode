fdccec4eb204ddb04a707ea3145abdd3a7ce7368
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-01-31 23:50:04 +0530

    Time: 2 ms (97.89%) | Memory: 47.4 MB (28.87%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<Integer> list;
+    public boolean findTarget(TreeNode root, int k) {
+        list = new ArrayList<>();
+        if(root == null) return false;
+
+        inorder(root);
+
+        int i = 0, j = list.size() -1;
+
+        while(i < j){
+            int sum = list.get(i) + list.get(j);
+            if(sum == k) return true;
+            else if(sum < k) i++;
+            else j--;
+        }
+        return false;
+    }
+    private void inorder(TreeNode root){
+        if(root == null) return;
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+}

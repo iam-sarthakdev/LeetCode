4ca8d68542b4ba1886498e0d450a2dd761631c33
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-02-02 15:24:55 +0530

    Time: 0 ms (100%), Space: 45.1 MB (61.82%) - LeetHub

---
+++ LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree-02-02-2026-15-24-53.java
@@ -0,0 +1,16 @@
+class Solution {
+    TreeNode prev = null;
+    public boolean isValidBST(TreeNode root) {
+        return inorder(root);
+    }
+    private boolean inorder(TreeNode node){
+        if(node == null) return true;
+        if(!inorder(node.left)) return false;
+
+        if(prev != null && node.val <= prev.val) return false;
+        prev = node;
+
+        return inorder(node.right);
+        
+    }
+}

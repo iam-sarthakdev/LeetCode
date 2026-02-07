8d6ded6223bdceff34db87aeaade253d4865ab72
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-02-07 16:26:58 +0530

    Time: 7 ms (68.97%) | Memory: 43.6 MB (98.30%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,34 +1,36 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] freq = new int[26];
+        int[] winFreq = new int[26];
 
-        if(s1.length() > s2.length())return false;
-
-        for(char c : s1.toCharArray()){
-            freq[c - 'a']++;
+        for(char ch : s1.toCharArray()){
+            freq[ch - 'a']++;
         }
-        int i = 0, j = 0, required = s1.length();
+        int i = 0, j = 0;
 
         while(j < s2.length()){
             char ch = s2.charAt(j);
+            winFreq[ch - 'a']++;
 
-            if(freq[ch - 'a'] > 0){
-                required--;
+            // window invalid
+            if(j - i + 1 > s1.length()){
+                char left = s2.charAt(i);
+                winFreq[left - 'a']--;
+                i++;
             }
-            freq[ch - 'a']--;
 
-            if(j - i + 1 == s1.length()){  // valid window
-            if(required == 0) return true;
-
-            char left = s2.charAt(i);
-            if(freq[left - 'a'] >= 0){
-                required++;
-            }
-            freq[left - 'a']++;
-            i++;
+            // valid window
+            if(j - i + 1 == s1.length()){
+                if(matches(freq, winFreq)) return true;
             }
             j++;
         }
         return false;
+    }
+    private boolean matches(int[] a, int[] b){
+        for(int i= 0; i < a.length; i++){
+            if(a[i] != b[i]) return false;
+        }
+        return true;
     }
 }

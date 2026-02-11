1a2b18ecef282e81786d8edc6a36f98d5aba4a8b
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-02-12 00:15:16 +0530

    Time: 5 ms (75.16%) | Memory: 46.1 MB (50.77%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int dup=x;
+        while(x>0){
+            int last=x%10;
+            rev=rev*10+last;
+            x=x/10;
+        }
+        return rev==dup;
+    }
+}

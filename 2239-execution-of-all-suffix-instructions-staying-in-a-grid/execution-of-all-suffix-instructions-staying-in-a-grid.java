c3de87383bfdb5e607fc358638422f818c3b0676
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-02-19 00:26:44 +0530

    Time: 25 ms (35.26%) | Memory: 46.2 MB (99.42%) - LeetSync

--- 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.java
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.java
@@ -1,25 +1,22 @@
 class Solution {
-    public int[] executeInstructions(int n, int[] pos, String s) {
-        int len=s.length();
-        int result[]=new int[len];
-        int row=pos[0];
-        int col=pos[1];
-        int count=0;
-        for(int i=0;i<len;i++){
-            row=pos[0];
-            col=pos[1];
-            count=0;;
-            for(int j=i;j<len;j++){
-                char ch=s.charAt(j);
-                if(ch=='U')row--;
-                else if(ch=='D')row++;
-                else if(ch=='L')col--;
-                else col++;
-                if(row<0||col<0||row>=n||col>=n)break;
+    public int[] executeInstructions(int n, int[] startPos, String s) {
+        int m=s.length();
+        int[] res=new int[m];
+        
+        for(int i=0;i<m;i++){
+            int count=0,a=startPos[0],b=startPos[1];
+            for(int j=i;j<m;j++){
+                char c=s.charAt(j);
+                if(c=='R') b++;
+                if(c=='L') b--;
+                if(c=='U') a--;
+                if(c=='D') a++;
+                if(a<0 || a>=n || b<0 || b>=n) break;
                 count++;
             }
-            result[i]=count;
+            res[i]=count;
         }
-        return result;
+        return res;
+
     }
 }

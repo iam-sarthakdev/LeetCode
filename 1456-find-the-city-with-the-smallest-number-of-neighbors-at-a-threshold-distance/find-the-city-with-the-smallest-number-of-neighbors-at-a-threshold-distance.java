b8bcb6a3eb625a0eaeba99d687c3ab6e7471459b
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-02-19 23:17:37 +0530

    Time: 30 ms (25.14%) | Memory: 47.1 MB (17.98%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,61 +1,62 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-
         ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        int ans = -1;
+        int minReachable = Integer.MAX_VALUE;
         for(int i = 0; i < n; i++){
             adj.add(new ArrayList<>());
         }
-
-        for(int[] neigh : edges){
-            int u = neigh[0];
-            int v = neigh[1];
-            int wt = neigh[2];
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
 
             adj.get(u).add(new int[]{v,wt});
             adj.get(v).add(new int[]{u,wt});
         }
-        int ans = -1;
-        int minCity = Integer.MAX_VALUE;
 
         for(int city = 0; city < n; city++){
             int[] dist = dijkstra(city, adj, n);
 
-            int reachableCity = 0;
+            int cities = 0;
             for(int i = 0; i < n; i++){
-                if(i != city && dist[i] <= distanceThreshold){
-                    reachableCity++;
+                if(dist[i] <= distanceThreshold && i != city){
+                    cities++;
                 }
             }
-
-            if(reachableCity <= minCity){
-                minCity = reachableCity;
-                ans = city;
-            }
+                if(cities <= minReachable){
+                    ans = city;
+                    minReachable = cities;
+                }
         }
         return ans;
     }
     private int[] dijkstra(int src, ArrayList<ArrayList<int[]>> adj, int n){
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
         Queue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));
-            int[] dist = new int[n];
-            Arrays.fill(dist, Integer.MAX_VALUE);
-            dist[src] = 0;
-            pq.add(new int[]{0, src});
+        pq.add(new int[]{0, src});
 
-            while(!pq.isEmpty()){
-                int[] curr = pq.poll();
-                int currDist = curr[0];
-                int node = curr[1];
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
 
-                for(int[] neigh : adj.get(node)){
-                    int nextNode = neigh[0];
-                    int cost = neigh[1];
+            int dis = curr[0];
+            int node = curr[1];
 
-                    if(currDist + cost < dist[nextNode]){
-                        dist[nextNode] = currDist + cost;
-                        pq.add(new int[]{dist[nextNode], nextNode});
-                    }
+            if(dist[node] < dis) continue;
+
+            for(int[] neigh : adj.get(node)){
+                int nextNode = neigh[0];
+                int cost = neigh[1];
+
+                if(cost + dis < dist[nextNode]){
+                    dist[nextNode] = cost + dis;
+                    pq.add(new int[]{dist[nextNode], nextNode});
                 }
             }
-            return dist;
+        }
+        return dist;
     }
 }

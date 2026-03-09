e7fa25bc29c66a4fa93a8811d97c5d1ba7dab7ee
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-03-09 19:40:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii-03-09-2026-19-40-01.java
@@ -0,0 +1,24 @@
+class Solution {
+    Integer[][] dp;
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        dp = new Integer[n+1][amount+1];
+        return solve(amount,coins, n);
+    }
+    private int solve(int amount, int[] coins, int n){
+        // dp = new int[n+1][amount+1];
+        if(amount == 0) return 1;
+        if(n == 0) return 0;
+
+        if(dp[n][amount] != null) return dp[n][amount];
+        
+        if(amount < coins[n-1]){
+            return solve(amount, coins, n-1);
+        }else{
+            int notTake = solve(amount, coins, n-1);
+            int take = solve(amount - coins[n-1], coins, n);
+            dp[n][amount] = take + notTake;
+        }
+        return dp[n][amount];
+    }
+}

eacc0ddef3762090f4d5f42ed0cb5660a6070bd5
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-03-09 19:57:25 +0530

    Time: 5 ms (99.74%) | Memory: 42.7 MB (98.76%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,24 +1,14 @@
 class Solution {
-    Integer[][] dp;
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        dp = new Integer[n+1][amount+1];
-        return solve(amount,coins, n);
-    }
-    private int solve(int amount, int[] coins, int n){
-        // dp = new int[n+1][amount+1];
-        if(amount == 0) return 1;
-        if(n == 0) return 0;
+        int[] dp = new int[amount + 1];
 
-        if(dp[n][amount] != null) return dp[n][amount];
-        
-        if(amount < coins[n-1]){
-            return solve(amount, coins, n-1);
-        }else{
-            int notTake = solve(amount, coins, n-1);
-            int take = solve(amount - coins[n-1], coins, n);
-            dp[n][amount] = take + notTake;
+        dp[0] = 1;
+        for(int i = 0; i < coins.length; i++){
+            int coin = coins[i];
+            for(int a = coin; a <= amount; a++){
+                dp[a] += dp[a-coin];
+            }
         }
-        return dp[n][amount];
+        return dp[amount];
     }
 }

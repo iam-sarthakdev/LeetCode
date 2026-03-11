61ef16c46f7d57f0dfa2a677854a19e17d5dec0d
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-03-12 00:40:40 +0530

    Time: 0 ms (100%), Space: 46.4 MB (48.3%) - LeetHub

---
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii-03-12-2026-00-40-38.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int currentProfit = 0;
+        int buyPrice = Integer.MAX_VALUE;
+
+        for(int price : prices){
+
+            if(price < buyPrice) buyPrice = price;
+
+            if(price > buyPrice){
+                currentProfit += price - buyPrice;
+                buyPrice = price;
+            }
+            maxProfit = Math.max(maxProfit, currentProfit);
+        }
+        return maxProfit;
+    }
+}

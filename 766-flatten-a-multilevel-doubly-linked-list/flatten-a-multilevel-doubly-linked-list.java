0f59c4e8201baaa11197620270f9e5114b0fb814
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-04-03 15:33:34 +0530

    Time: 3 ms (5.30%) | Memory: 43.6 MB (28.06%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -1,13 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node prev;
-    public Node next;
-    public Node child;
-};
-*/
-
 class Solution {
     public Node flatten(Node head) {
         Node temp = head;

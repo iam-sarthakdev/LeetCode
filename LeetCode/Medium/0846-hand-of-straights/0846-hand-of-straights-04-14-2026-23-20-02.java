890961650daa3016bccf69869386b6a3aa0a4264
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-04-14 23:20:02 +0530

    04-14-2026 Medium .java Array 0846-hand-of-straights N/A (0%) N/A (0%)

---
+++ LeetCode/Medium/0846-hand-of-straights/0846-hand-of-straights-04-14-2026-23-20-02.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+
+        if(n % groupSize != 0) return false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num : hand){
+            map.put(num, map.getOrDefault(num, 0) +1);
+        }
+
+        Queue<Integer> minHeap = new PriorityQueue<>(map.keySet());
+
+        while(!minHeap.isEmpty()){
+            int start = minHeap.peek();
+
+            for(int i = 0; i < groupSize; i++){
+                int curr = start + i;
+                if(!map.containsKey(curr)) return false;
+
+                map.put(curr, map.get(curr) - 1);
+
+                if(map.get(curr) == 0){
+                    map.remove(curr);
+
+                    // if(minHeap.peek() != curr) return false;
+
+                    minHeap.poll();
+                }
+            }
+        }
+        return true;
+    }
+}

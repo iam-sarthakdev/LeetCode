74138a34c19da0792f903a2f0f0f05b9d30ea6e8
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-04-16 23:30:44 +0530

    04-16-2026 Medium .java Array 0189-rotate-array N/A (0%) N/A (0%)

---
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array-04-16-2026-23-30-44.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public void reverse(int[] nums, int i, int j){
+        while(i < j){
+        nums[i] = nums[i] ^ nums[j];
+        nums[j] = nums[i] ^ nums[j];
+        nums[i] = nums[i] ^ nums[j];
+        i++;
+        j--;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0 , n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+        
+    }
+}

26afa96cb7e2cd0d5dcc9d1e1b87ac30903b9ea4
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-04-24 03:02:35 +0530

    04-24-2026 Medium .java Math 0050-powx-n N/A (0%) N/A (0%)

---
+++ LeetCode/Medium/0050-powx-n/0050-powx-n-04-24-2026-03-02-34.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double res = 1;
+
+        long N = n;
+
+        if(N < 0){
+            x = 1 / x;
+            N = -N;
+        }
+
+        while(N > 0){
+            if((N % 2) == 1){ // odd
+                res = res * x;
+            }
+            x = x * x;
+            N = N /2;
+        }
+        return res;
+    }
+}

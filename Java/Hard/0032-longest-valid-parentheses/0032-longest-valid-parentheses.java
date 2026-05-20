6f451a2dbf2b846f8ebc16022b9196870d65cc3b
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-05-21 03:06:35 +0530

    String 0032-longest-valid-parentheses Hard 05-21-2026

---
+++ Java/Hard/0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        if(s.length() == 0) return 0;
+
+        int maxLen = 0;
+        Stack<Integer> st = new Stack<>();
+
+        st.push(-1);
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '('){
+                st.push(i);
+            }else{
+                st.pop();
+                if(st.isEmpty()){
+                    st.push(i);
+                }else{
+                    maxLen = Math.max(maxLen, i- st.peek());
+                }
+            }
+        }
+        return maxLen;
+    }
+}

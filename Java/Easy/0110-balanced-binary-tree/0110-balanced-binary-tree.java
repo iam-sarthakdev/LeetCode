7058a9d623c8dbd3c280f7de8d913b4c0f9c05e7
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-06-02 01:41:20 +0530

    Tree 0110-balanced-binary-tree Easy 06-02-2026

---
+++ Java/Easy/0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean isBalanced = true;
+    public boolean isBalanced(TreeNode root) {
+        height(root);
+        return isBalanced;
+    }
+    private int height(TreeNode root){
+        
+        if(root == null) return 0;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if(Math.abs(left - right) > 1){
+            isBalanced = false;
+        }
+        return 1 + Math.max(left, right);
+    }
+}

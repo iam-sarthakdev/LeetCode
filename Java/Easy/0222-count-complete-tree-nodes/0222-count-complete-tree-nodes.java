3af0abbb252e5e20ab47fd47a7ae73e85427ede8
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-06-12 23:10:47 +0530

    Binary Search 0222-count-complete-tree-nodes Easy 06-12-2026

---
+++ Java/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+
+        int leftHeight = getLeft(root);
+        int rightHeight = getRight(root);
+
+        if(leftHeight == rightHeight){ // perfect tree
+            return (1 << leftHeight)-1;
+        }
+        // else recurse
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    private int getLeft(TreeNode root){
+        int height = 0;
+        while(root != null){
+            height++;
+            root = root.left;
+        }
+        return height;
+    }
+    private int getRight(TreeNode root){
+        int height = 0;
+        while(root != null){
+            height++;
+            root = root.right;
+        }
+        return height;
+    }
+}

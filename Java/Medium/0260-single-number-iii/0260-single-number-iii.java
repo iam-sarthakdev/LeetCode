26330640db64a5c023d0560b4be1865a3b6d6a36
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-06-15 23:11:04 +0530

    Array 0260-single-number-iii Medium 06-15-2026

---
+++ Java/Medium/0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for(int num : nums){
+            xor ^= num;
+        }
+        int mask = xor & (-xor);
+
+        // seperate into two groups
+        int num1 = 0;
+        int num2 = 0;
+
+        for(int num : nums){
+            if((num & mask) != 0){
+                num1 ^= num;
+            }else{
+                num2 ^= num;
+            }
+        }
+        return new int[]{num1,num2};
+    }
+}

a83f9737ab61a0a42ad13379c7952a79e24cd6bb
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-06-20 23:31:53 +0530

    Linked List 0445-add-two-numbers-ii Medium 06-20-2026

---
+++ Java/Medium/0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> st1 = new Stack<>();
+        Stack<Integer> st2 = new Stack<>();
+        ListNode dummy = new ListNode(-1);
+
+        while(l1 != null){
+            st1.push(l1.val);
+            l1 = l1.next;
+        }
+        while(l2 != null){
+            st2.push(l2.val);
+            l2 = l2.next;
+        }
+        int carry = 0;
+        while(!st1.isEmpty() || !st2.isEmpty() || carry != 0){
+           int sum = carry;
+            if(!st1.isEmpty()){
+                sum+= st1.pop();
+            }
+            if(!st2.isEmpty()){
+                sum+= st2.pop();
+            }
+        //    int digit = sum % 10;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            node.next = dummy.next;
+            dummy.next = node;
+        }
+        return dummy.next;
+    }
+}

559953502763977c636fc6736d56b6554bb4018e
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-07-03 23:17:46 +0530

    Stack 0901-online-stock-span Medium 07-03-2026

---
+++ Java/Medium/0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,16 @@
+class StockSpanner {
+    Deque<int[]> st;
+    public StockSpanner() {
+        st = new ArrayDeque<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+
+        while(!st.isEmpty() && st.peek()[0] <= price){
+            span += st.pop()[1];
+        }
+        st.push(new int[] {price, span});
+        return span;
+    }
+}

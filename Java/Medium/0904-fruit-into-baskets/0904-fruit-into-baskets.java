acd91a5ca95e80f6eb87ac2d6888e9b81a4999ce
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-07-03 23:18:14 +0530

    Array 0904-fruit-into-baskets Medium 07-03-2026

---
+++ Java/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int maxLen = 0;
+        int i = 0, j = 0;
+        while(j < fruits.length){
+            map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+
+            while(map.size() > 2){
+                 map.put(fruits[i],map.get(fruits[i])-1);
+                 if(map.get(fruits[i]) == 0) map.remove(fruits[i]);
+                 i++;
+            }
+            maxLen = Math.max(maxLen, j - i + 1);
+            j++;
+        }
+        return maxLen;
+    }
+}

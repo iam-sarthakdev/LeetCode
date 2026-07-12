32525e78a6094bb3214bf69ed8b174fe127fdef6
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-07-12 23:11:44 +0530

    Time: 7 ms (40.18%) | Memory: 46.3 MB (91.78%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -7,16 +7,16 @@ class Solution {
         });
         int start = intervals[0][0];
         int end = intervals[0][1];
-        int count = 0;
+        int removed = 0;
 
         for(int i = 1; i < n; i++){
             if(intervals[i][0] >= start && intervals[i][1] <= end){
-                count++;
+                removed++;
             }else{
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
         }
-        return n - count;
+        return n - removed;
     }
 }

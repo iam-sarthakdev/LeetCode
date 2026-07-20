bb925ee67d4f7db8a510024425976b8e55ee2a61
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-07-20 22:46:54 +0530

    Time: 713 ms (8.47%) | Memory: 43.4 MB (83.62%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -1,24 +1,30 @@
 class Solution {
-
     public int nextBeautifulNumber(int n) {
-        for (int i = n + 1; i <= 1224444; ++i) {
-            if (isBalance(i)) {
-                return i;
-            }
+        List<Integer> list = new ArrayList<>();
+        generate(0, new int[10], list);
+        Collections.sort(list);
+        for (int num : list) {
+            if (num > n) return num;
         }
         return -1;
     }
-
-    private boolean isBalance(int x) {
-        int[] count = new int[10];
-        while (x > 0) {
-            count[x % 10]++;
-            x /= 10;
+    private void generate(long num, int[] count, List<Integer> list) {
+        if (num > 0 && isBeautiful(count)) {
+            list.add((int) num);
         }
-        for (int d = 0; d < 10; ++d) {
-            if (count[d] > 0 && count[d] != d) {
-                return false;
+        if (num > 1224444) return;
+
+        for (int d = 1; d <= 7; d++) {
+            if (count[d] < d) {
+                count[d]++;
+                generate(num * 10 + d, count, list);
+                count[d]--;
             }
+        }
+    }
+    private boolean isBeautiful(int[] count) {
+        for (int d = 1; d <= 7; d++) {
+            if (count[d] != 0 && count[d] != d) return false;
         }
         return true;
     }

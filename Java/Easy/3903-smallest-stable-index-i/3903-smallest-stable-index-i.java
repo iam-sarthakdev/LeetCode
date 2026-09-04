433751ed650d63f12e12b9be36b2f624058bffde
Sarthak Kanoi <sarthak1712005@gmail.com>
2026-09-04 13:12:53 +0530

    Array 3903-smallest-stable-index-i Easy 09-04-2026

--- Java/Easy/3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ Java/Easy/3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -4,20 +4,21 @@ class Solution {
         long max = Integer.MIN_VALUE;
         long min = Integer.MAX_VALUE;
 
-        long[] maxi = new long[n];
+        // long[] maxi = new long[n];
         long[] mini = new long [n];
 
-        for(int i = 0; i < n; i++){
-            max = Math.max(max, nums[i]);
-            maxi[i] = max;
-        }
+        // for(int i = 0; i < n; i++){
+        //     max = Math.max(max, nums[i]);
+        //     maxi[i] = max;
+        // }
         for(int i = n-1; i >= 0; i--){
             min = Math.min(min, nums[i]);
             mini[i] = min;
         }
 
         for(int i = 0; i < n; i++){
-            if(maxi[i] - mini[i] <= k){
+            max = Math.max(max, nums[i]);
+            if(max - mini[i] <= k){
                 return i;
             }
         }
